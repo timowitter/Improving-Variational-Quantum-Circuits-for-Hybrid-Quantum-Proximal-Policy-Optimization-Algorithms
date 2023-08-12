97e4c376d124cca0d159d3d7bfb0b14e2c53b8f6
TimoWitter <timowitter@aol.com>
2023-08-12 12:05:06 +0200

    new tests

--- test_log.py
+++ test_log.py
@@ -329,12 +329,12 @@ exp_names = [
     "qppo-simple_reuploading-qlr0.5e-3-6varlayers-nologoutput-allmid-recordgrads-(72-params)",
     "qppo-simple_reuploading-qlr0.5e-3-6varlayers-nologoutput-allbig-recordgrads-(72-params)",
     "qppo-simple_reuploading-qlr0.5e-3-6varlayers-nologoutput-allverysmall-recordgrads-(72-params)",
-    "qppo-simple_reuploading-qlr0.5e-3-6varlayers-nologoutput-alltoosmall-recordgrads-(72-params)",
+    # "qppo-simple_reuploading-qlr0.5e-3-6varlayers-nologoutput-alltoosmall-recordgrads-(72-params)",
     "random-baseline",
 ]
 seeds = [10, 20, 30]
 stepsize = 4 * 128 * 8
-max_steps = 350000
+max_steps = 500000
 
 plot_test_avg(results_dir, plot_dir, gym_id, exp_names, seeds, stepsize, max_steps)
 
@@ -344,6 +344,6 @@ exp_names = [
     "qppo-simple_reuploading-qlr0.5e-3-6varlayers-nologoutput-allmid-recordgrads-(72-params)",
     "qppo-simple_reuploading-qlr0.5e-3-6varlayers-nologoutput-allbig-recordgrads-(72-params)",
     "qppo-simple_reuploading-qlr0.5e-3-6varlayers-nologoutput-allverysmall-recordgrads-(72-params)",
-    "qppo-simple_reuploading-qlr0.5e-3-6varlayers-nologoutput-alltoosmall-recordgrads-(72-params)",
+    # "qppo-simple_reuploading-qlr0.5e-3-6varlayers-nologoutput-alltoosmall-recordgrads-(72-params)",
 ]
 plot_gradient_avg(results_dir, plot_dir, gym_id, exp_names, seeds, max_steps)

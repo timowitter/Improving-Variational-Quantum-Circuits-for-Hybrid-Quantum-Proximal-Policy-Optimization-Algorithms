4ea25732b2d415c138848e643a28644a82ca5336
Timo_Witter <timowitter@aol.com>
2023-09-06 10:52:13 +0200

    new tests

--- src/env_setup.py
+++ src/env_setup.py
@@ -1,11 +1,14 @@
 import gymnasium as gym
 
-#Chen's shortest path frozen lake (from https://github.com/ycchen1989/Var-QuantumCircuits-DeepRL/tree/master/Code)
+# Chen's shortest path frozen lake (from https://github.com/ycchen1989/Var-QuantumCircuits-DeepRL/tree/master/Code)
 from gymnasium.envs.registration import register
+
 register(
-    id='Deterministic-ShortestPath-4x4-FrozenLake-v0', # name given to this new environment
-    entry_point='ShortestPathFrozenLake:ShortestPathFrozenLake', # env entry point
-    kwargs={'desc': ["SFFF", "FFFH", "FHFH", "HFFG"], 'map_name': '4x4', 'is_slippery': False} # argument passed to the env
+    id="Deterministic-ShortestPath-4x4-FrozenLake-v0",  # name given to this new environment
+    entry_point="ShortestPathFrozenLake:ShortestPathFrozenLake",  # env entry point
+    # kwargs={'desc': ["SFFF", "FFFH", "FHFH", "HFFG"], 'map_name': '4x4', 'is_slippery': False} # argument passed to the env
+    # if you want to use alternate Frozen Lake env for Output Scaling Bias Test:
+    kwargs={"desc": ["SFFF", "HHFF", "FGHF", "FFFF"], "map_name": "4x4", "is_slippery": False},
 )
 # register(
 #     id='Deterministic-4x4-FrozenLake-v0', # name given to this new environment
@@ -14,38 +17,46 @@ register(
 # )
 
 
-#environment setup:
-def make_env(gym_id, seed, env_num, capture_video, run_name, num_envs, chkpt_dir, load_chkpt, store_envs):
+# environment setup:
+def make_env(
+    gym_id, seed, env_num, capture_video, run_name, num_envs, chkpt_dir, load_chkpt, store_envs
+):
     def thunk():
         if gym_id == "Deterministic-ShortestPath-4x4-FrozenLake-v0":
-            env = gym.make('Deterministic-ShortestPath-4x4-FrozenLake-v0')
+            env = gym.make("Deterministic-ShortestPath-4x4-FrozenLake-v0")
             print("ShortestPathFrozenLake")
             env = gym.wrappers.TimeLimit(env, max_episode_steps=100)
-        elif (gym_id == "FrozenLake-v1"):
-            env = gym.make(gym_id, desc=["SFFF", "FFFH", "FHFH", "HFFG"], map_name="4x4", is_slippery=False)
+        elif gym_id == "FrozenLake-v1":
+            env = gym.make(
+                gym_id, desc=["SFFF", "FFFH", "FHFH", "HFFG"], map_name="4x4", is_slippery=False
+            )
             print("is_slippery=False")
             env = gym.wrappers.TimeLimit(env, max_episode_steps=100)
         else:
             env = gym.make(gym_id)
         env = gym.wrappers.RecordEpisodeStatistics(env)  # VectorListInfo
         if capture_video:
-            if env_num==0:
-                env = gym.wrappers.RecordVideo(env, f"videos/{run_name}", record_video_trigger=lambda t: t % 1000 == 0)
-        if not (gym_id == "FrozenLake-v1" or gym_id == "Deterministic-ShortestPath-4x4-FrozenLake-v0"):
+            if env_num == 0:
+                env = gym.wrappers.RecordVideo(
+                    env, f"videos/{run_name}", record_video_trigger=lambda t: t % 1000 == 0
+                )
+        if not (
+            gym_id == "FrozenLake-v1" or gym_id == "Deterministic-ShortestPath-4x4-FrozenLake-v0"
+        ):
             env.seed(seed)
         env.action_space.seed(seed)
         env.observation_space.seed(seed)
 
-        if (load_chkpt and store_envs.restore_envs[env_num]):
+        if load_chkpt and store_envs.restore_envs[env_num]:
             env.reset()
             store_envs.load_envs(chkpt_dir, num_envs)
             checkpoint_aktions = store_envs.get_storage(env_num)
-            if (checkpoint_aktions.size == 1):
+            if checkpoint_aktions.size == 1:
                 env.step(int(checkpoint_aktions))
             else:
                 for j in range(checkpoint_aktions.size):
                     env.step(checkpoint_aktions[j])
             store_envs.restore_envs[env_num] = False
         return env
-    return thunk
 
+    return thunk

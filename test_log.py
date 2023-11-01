0cb0304e54897489fee54095c75fb225a1421668
Timo_Witter <timowitter@aol.com>
2023-11-01 17:41:18 +0100

    new tests

--- test_log.py
+++ test_log.py
@@ -1452,7 +1452,7 @@ max_steps = 150000
 alpha = 0.05
 
 plot_test_avg_final(results_dir, plot_dir, gym_id, exp_names, seeds, alpha, max_steps)
-"""
+
 
 plot_dir = plots_dir + "/FL-actor-Hyperparameter-Test-0-NN(4)-lr"
 gym_id = "Deterministic-ShortestPath-4x4-FrozenLake-v0"
@@ -1509,6 +1509,25 @@ exp_names = [
     "FL-ppo-ac-NN(64,64)-(actor-lr=2.5e-4)-(5508-params)",
     "FL-ppo-ac-NN(64,64)-(actor-lr=1.0e-3)-(5508-params)",
     "random-baseline",
+]
+seeds = [10, 20, 30]
+batchsize = 4 * 128
+max_steps = 150000
+alpha = 0.05
+
+plot_test_avg_final(results_dir, plot_dir, gym_id, exp_names, seeds, alpha, max_steps)
+"""
+
+plot_dir = plots_dir + "/FL-actor-Hyperparameter-Test-0b-best-NN"
+gym_id = "Deterministic-ShortestPath-4x4-FrozenLake-v0"
+exp_names = [
+    "FL-ppo-ac-NN(3)-(actor-lr=5.0e-2)-(67-params)",
+    "FL-ppo-ac-NN(4)-(actor-lr=5.0e-2)-(88-params)",
+    "FL-ppo-ac-NN(5)-(actor-lr=5.0e-2)-(109-params)",
+    "FL-ppo-ac-NN(4,4)-(actor-lr=1.0e-2)-(108-params)",
+    "FL-ppo-ac-NN(64,64)-(actor-lr=1.0e-3)-(5508-params)",
+    "random-baseline",
+    "ppo_default_lr100.0e-4",
 ]
 seeds = [10, 20, 30]
 batchsize = 4 * 128
